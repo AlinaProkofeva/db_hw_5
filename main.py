9021222b9db9daef5d373fce92671c818509e0b5
Алина Прокофьева <ve4nayavesna@mail.ru>
2022-09-29 00:30:44 +0300

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+
+import psycopg2
+
+if __name__ == '__main__':
+    print(f'''НЕНАВИЖУ ВСКОД!!!!
+    ''')
+
+for i in range(4):
+    print(i)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
